02265d11f7446dba04d8812e88f35c0aa46beb2b
rayjamil <ryanwekesa019@gmail.com>
2025-01-20 16:06:40 +0300

    Adds multiple files

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+
 
 function App() {
   return (
@@ -11,3 +15,4 @@ function App() {
 }
 
 export default App;
+
